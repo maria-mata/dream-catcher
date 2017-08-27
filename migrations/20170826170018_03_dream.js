50af21c057cceb0be3aa60b2f7cfbaa61a196ee0
Maria Mata <mariajo.mata@gmail.com>
2017-08-27 10:56:16 -0600

    ran migrations

--- migrations/20170826170018_03_dream.js
+++ migrations/20170826170018_03_dream.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('dream', (table) => {
   table.increments();
-  table.date('date').notNullable().defaultTo(new Date);
+  table.date('date').notNullable().defaultTo(knex.raw('now()'));
   table.text('description').notNullable();
   table.integer('user_id').references('user.id').unsigned().onDelete('cascade');
   table.integer('category_id').references('category.id').notNullable();
