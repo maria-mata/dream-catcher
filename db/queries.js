4adf1994cc7ac8ae2d54310fd80c90881f22ea70
Maria Mata <mariajo.mata@gmail.com>
2017-08-27 14:10:14 -0600

    created add dream post route & query

--- db/queries.js
+++ db/queries.js
@@ -1,9 +1,17 @@
 const knex = require('./knex')
 
 module.exports = {
+  getDreams: () => {
+    return knex('dream')
+    .join('category', 'dream.category_id', 'category.id')
+    .select('dream.*', 'category.category')
+  },
   getDreamsByUserId: userId => {
     return knex('dream').where('dream.user_id', userId)
       .join('category', 'dream.category_id', 'category.id')
       .select('dream.*', 'category.category')
+  },
+  addDream: dream => {
+    return knex('dream').insert(dream).returning('*')
   } // add more queries here
 }
--- routes/dreams.js
+++ routes/dreams.js
@@ -3,6 +3,14 @@ const router = express.Router();
 const knex = require('../db/knex');
 const queries = require('../db/queries')
 
+// GET all dreams
+router.get('/', (req, res) => {
+  queries.getDreams()
+  .then(dreams => {
+    res.json(dreams)
+  })
+})
+
 // GET all dreams for one user by ID
 router.get('/:id', (req, res) => {
   queries.getDreamsByUserId(req.params.id)
@@ -10,5 +18,13 @@ router.get('/:id', (req, res) => {
     res.json(dreams) // change to handlebars render later
   })
 });
+
+// POST a new dream
+router.post('/', (req, res) => {
+  queries.addDream(req.body)
+  .then(() => {
+    res.json({message: 'Success!'})
+  })
+})
 
 module.exports = router;
