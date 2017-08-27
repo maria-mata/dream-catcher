f4dfd984dc6341a6ab2616c2781290dd217a9848
Maria Mata <mariajo.mata@gmail.com>
2017-08-27 16:00:32 -0600

    fixed queries due to re-seeding

--- db/queries.js
+++ db/queries.js
@@ -19,13 +19,13 @@ module.exports = {
   getDreams: () => {
     return knex('dream')
     .join('category', 'dream.category_id', 'category.id')
-    .select('dream.*', 'category.category')
+    .select('dream.*', 'category.name AS category_name')
   },
 
   getDreamsByUserId: userId => {
     return knex('dream').where('dream.user_id', userId)
       .join('category', 'dream.category_id', 'category.id')
-      .select('dream.*', 'category.category')
+      .select('dream.*', 'category.name AS category_name')
   },
 
   getCategories: () => {
